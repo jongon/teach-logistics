f66159a9deb37034752ae28d1ad1e679b031fba3
Jonathan González <jon.gon1@gmail.com>
2015-04-08 11:04:36 -0430

    Project fixes, Intelissence fixed too

--- Tesis/Scripts/_reference.js
+++
@@ -1,5 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="modernizr/modernizr-2.8.3.js" />
-/// <reference path="jquery/jquery-2.1.3.js" />
-/// <reference path="bootstrap/bootstrap.js" />
-/// <reference path="respondjs/respond.js" />
