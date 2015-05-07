fd604c814f9a8d61ded126cd4a786c4a2bce8f70
Jonathan González <jon.gon1@gmail.com>
2015-05-06 23:13:39 -0430

    Nuevos Modelos Sale/Period

---
+++ Tesis/Models/Period.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tesis.Models
+{
+    public class Period
+    {
+        public Guid Id { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public byte PeriodNumber { get; set; }
+
+        public bool isLastPeriod { get; set; }
+
+        public Section Section { get; set; }
+
+        public Guid SectiondId { get; set; }
+
+        public ICollection<Sale> Sales { get; set; }
+    }
+}
---
+++ Tesis/Models/Sale.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tesis.Models
+{
+    public class Sale
+    {
+        public Guid Id { get; set; }
+
+        public int Quantity { get; set; }
+
+        public Period Period { get; set; }
+
+        public Guid PeriodId { get; set; }
+
+        public Product Product { get; set; }
+
+        public Guid ProductId { get; set; }
+    }
+}
