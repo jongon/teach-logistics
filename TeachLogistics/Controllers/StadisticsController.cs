01a4e8a7da73c6557a6195563bf3b0a583b18724
Jonathan González <jon.gon1@gmail.com>
2015-07-01 12:26:48 -0430

    Release v1.0b

--- TeachLogistics/Controllers/StadisticsController.cs
+++ TeachLogistics/Controllers/StadisticsController.cs
@@ -14,15 +14,6 @@ namespace TeachLogistics.Controllers
 {
     public class StadisticsController : BaseController
     {
-        // GET: Stadistics
-        [Authorize(Roles = "Administrador")]
-        [HttpGet]
-        public async Task<ActionResult> Index()
-        {
-            return View();
-        }
-
-
         [Authorize(Roles = "Estudiante")]
         [HttpGet]
         public ActionResult StudentStadistics()
